04f41a33884228321c03208ca94dd4b1a0e5af92
Botond70 <97360095+Botond70@users.noreply.github.com>
2024-09-30 14:35:15 +0200

    Update ngrokmanager.py

--- ngrokmanager.py
+++ ngrokmanager.py
@@ -70,7 +70,7 @@ def inject(token):
 
     
 def drop(counter: int,tokens):
-    #drops n tokens from the list, updates the counter accoridingly
+    #drops nth token from the list, updates the counter accoridingly
     tokens.pop(c)
     counter %= len(tokens)
     return counter, tokens
@@ -91,27 +91,32 @@ def main():
             arg2 = int(sys.argv[2])
         except:
             arg2 = 1
-        
+
+        #b = load from backup
         if "b" in arg:
             counter = 0
             tokens = loadbackup()
-            
+
+        #d = drop arg2-th token
         if "d" in arg:
             if len(sys.argv) > 2:
                 drop(arg2,tokens)
                 
             else:
                 drop(counter,tokens)
-                
+        #cycle onto next token
         if "n" in arg:
             for i in range(arg2):
                 counter = next(counter,tokens)
                 print(counter,"\n")
-                
+        #inject the last used token (from previous run)     
         if "i" in arg: inject(tokens[counter])
         
     else:
+        #cli text ui mode
+        #"q" quits the program and saves the counter, and updates the token list file ("tokens.txt").
         while ((command := input("Type: ")) != "q"):
+            #load tokens
             if command == "l":
                 tokens = load()
                 print(tokens)
@@ -125,7 +130,7 @@ def main():
             elif command == "d": #drop current token
                 drop(counter,tokens)
 
-    
+    #saves the counter, and tokens into their respective files.
     print("\nlogging counter and tokens")
     with open("counter.txt","w") as c:
         c.write(str(counter))
